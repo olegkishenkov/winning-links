c750eed40bd22ef9e9e29e6ff6f8f19084ba5543
oleg1248 <oleg1248163264@gmail.com>
2020-05-22 01:25:51 +0300

    passes 7 tests with one client

--- tests.py
+++ tests.py
@@ -4,16 +4,122 @@ from winning_links import find_winning_links as fwl
 
 
 class TestFindWinningLinks(unittest.TestCase):
-    def test_find_winning_links_search_engine_shop(self):
-        log_string = '''{
-    "client_id": "user15",
-    "User-Agent": "Firefox 59",
-    "document.location": "https://shop.com/checkout",
-    "document.referer": "https://yandex.ru/search/?q=купить+котика",
-    "date": "2018-04-03T07:59:13.286000Z"
-}'''
-        log_string = '''[
+    def test_find_winning_links__search_engine_to_checkout(self):
+        log_str = '''    {
+        "client_id": "user15",
+        "User-Agent": "Firefox 59",
+        "document.location": "https://shop.com/checkout",
+        "document.referer": "https://yandex.ru/search/?q=купить+котика",
+        "date": "2018-04-03T07:59:13.286000Z"
+    }'''
+        log_str = '''[
 {}
-]'''.format(log_string)
-        links = fwl(log_string)
+]'''.format(log_str)
+        links = fwl(log_str)
+        self.assertEqual(links, [])
+
+    def test_find_winning_links__ours_to_checkout(self):
+        log_str = '''[
+    {
+        "client_id": "user15",
+        "User-Agent": "Firefox 59",
+        "document.location": "https://shop.com/checkout",
+        "document.referer": "https://referal.ours.com/?ref=0xc0ffee",
+        "date": "2018-04-03T07:59:13.286000Z"
+    }
+]'''
+        links = fwl(log_str)
+        self.assertEqual(links, ['https://referal.ours.com/?ref=0xc0ffee'])
+
+    def test_find_winning_links__theirs1_to_checkout(self):
+        log_str = '''[
+    {
+        "client_id": "user15",
+        "User-Agent": "Firefox 59",
+        "document.location": "https://shop.com/checkout",
+        "document.referer": "https://ad.theirs1.com/?src=q1w2e3r4",
+        "date": "2018-04-03T07:59:13.286000Z"
+    }
+]'''
+        links = fwl(log_str)
+        self.assertEqual(links, [])
+
+    def test_find_winning_links__ours_to_theirs1_to_checkout(self):
+        log_str = '''[
+    {
+        "client_id": "user15",
+        "User-Agent": "Firefox 59",
+        "document.location": "https://ad.theirs1.com/?src=q1w2e3r4",
+        "document.referer": "https://referal.ours.com/?ref=0xc0ffee",
+        "date": "2018-04-02T07:59:13.286000Z"
+    },
+    {
+        "client_id": "user15",
+        "User-Agent": "Firefox 59",
+        "document.location": "https://shop.com/checkout",
+        "document.referer": "https://ad.theirs1.com/?src=q1w2e3r4",
+        "date": "2018-04-03T07:59:13.286000Z"
+    }
+]'''
+        links = fwl(log_str)
+        self.assertEqual(links, [])
+
+    def test_find_winning_links__theirs1_to_ours_to_checkout(self):
+        log_str = '''[
+    {
+        "client_id": "user15",
+        "User-Agent": "Firefox 59",
+        "document.location": "https://referal.ours.com/?ref=0xc0ffee",
+        "document.referer": "https://ad.theirs1.com/?src=q1w2e3r4",
+        "date": "2018-04-02T07:59:13.286000Z"
+    },
+    {
+        "client_id": "user15",
+        "User-Agent": "Firefox 59",
+        "document.location": "https://shop.com/checkout",
+        "document.referer": "https://referal.ours.com/?ref=0xc0ffee",
+        "date": "2018-04-03T07:59:13.286000Z"
+    }
+]'''
+        links = fwl(log_str)
+        self.assertEqual(links, ['https://referal.ours.com/?ref=0xc0ffee'])
+
+    def test_find_winning_links__ours_to_cart_to_checkout(self):
+        log_str = '''[
+    {
+        "client_id": "user15",
+        "User-Agent": "Firefox 59",
+        "document.location": "https://shop.com/cart",
+        "document.referer": "https://referal.ours.com/?ref=0xc0ffee",
+        "date": "2018-04-02T07:59:13.286000Z"
+    },
+    {
+        "client_id": "user15",
+        "User-Agent": "Firefox 59",
+        "document.location": "https://shop.com/checkout",
+        "document.referer": "https://shop.com/cart",
+        "date": "2018-04-03T07:59:13.286000Z"
+    }
+]'''
+        links = fwl(log_str)
+        self.assertEqual(links, ['https://referal.ours.com/?ref=0xc0ffee'])
+
+    def test_find_winning_links__theirs1_to_cart_to_checkout(self):
+        log_str = '''[
+    {
+        "client_id": "user15",
+        "User-Agent": "Firefox 59",
+        "document.location": "https://shop.com/cart",
+        "document.referer": "https://ad.theirs1.com/?src=q1w2e3r4",
+        "date": "2018-04-02T07:59:13.286000Z"
+    },
+    {
+        "client_id": "user15",
+        "User-Agent": "Firefox 59",
+        "document.location": "https://shop.com/checkout",
+        "document.referer": "https://shop.com/cart",
+        "date": "2018-04-03T07:59:13.286000Z"
+    }
+]'''
+        links = fwl(log_str)
         self.assertEqual(links, [])
--- winning_links.py
+++ winning_links.py
@@ -5,32 +5,40 @@ import dateutil.parser
 
 
 def is_checkout(link):
-    return urllib.parse.urlparse(link).hostname == 'https://shop.com/checkout'
+    result = urllib.parse.urlparse(link)
+    return (result.hostname == 'shop.com') and (result.path == '/checkout')
 
 
 def is_ours(link):
-    return _(link) == 'referral.ours'
+    result = urllib.parse.urlparse(link)
+    return result.hostname == 'referal.ours.com'
 
 
-def find_winning_links(log_string):
-    log = json.loads(log_string)
+def is_theirs(link):
+    result = urllib.parse.urlparse(link)
+    return (result.hostname == 'ad.theirs1.com') or (result.hostname == 'ad.theirs2.com')
+
+
+def find_winning_links(log_str):
+    log = json.loads(log_str)
     winning_links = []
-    for hop in filter(lambda _: is_checkout(['document.location']), log):
+    for hop in filter(lambda _: is_checkout(_['document.location']), log):
         log_this_client = list(filter(lambda _: _['client_id'] == hop['client_id'], log))
-        hop_this = log_this_client[0]
-        while True:
-            hop_pre = None
+        for hop_this in log_this_client:
+            winning_link_candidate = hop['document.referer']
             for hop_ in log_this_client:
-                if hop_['document.location'] == hop['document.referer']:
-                    if dateutil.parser.parse(hop_['date']) < dateutil.parser.parse(hop['date']):
-                        hop_pre = hop_
-            if is_ours(hop_pre['document.referrer']):
-                winning_links.append(hop_pre['document.referrer'])
+                if hop_['document.location'] == hop_this['document.referer']:
+                    if dateutil.parser.parse(hop_['date']) < dateutil.parser.parse(hop_this['date']):
+                        winning_link_candidate = hop_['document.referer']
+            if is_ours(winning_link_candidate):
+                winning_links.append(winning_link_candidate)
+                break
+            if is_theirs(winning_link_candidate):
                 break
     return winning_links
 
 
 if __name__ == '__main__':
     with open('log.json') as log_fh:
-        log_string = log_fh.read()
-    print(find_winning_links(log_string))
+        log_str = log_fh.read()
+    print(find_winning_links(log_str))
