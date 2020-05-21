43bc77d96560fb765e5424581373a5eabd4adf7d
oleg1248 <oleg1248163264@gmail.com>
2020-05-21 23:17:48 +0300

    put the logic into a function

--- winning_links.py
+++ winning_links.py
@@ -12,20 +12,25 @@ def is_ours(link):
     return _(link) == 'referral.ours'
 
 
-log = json.loads('log.json')
-winning_links = []
+def find_winning_links(log_file_name):
+    with open(log_file_name) as log_fh:
+        log = json.loads(log_fh.read())
 
-for hop in filter(lambda _: is_checkout(['document.location']), log):
-    log_this_client = list(filter(lambda _: _['client_id'] == hop['client_id'], log))
-    hop_this = log_this_client[0]
-    while True:
-        hop_pre = None
-        for hop_ in log_this_client:
-            if hop_['document.location'] == hop['document.referer']:
-                if dateutil.parser.parse(hop_['date']) < dateutil.parser.parse(hop['date']):
-                    hop_pre = hop_
-        if is_ours(hop_pre['document.referrer']):
-            winning_links.append(hop_pre['document.referrer'])
-            break
+    winning_links = []
+    for hop in filter(lambda _: is_checkout(['document.location']), log):
+        log_this_client = list(filter(lambda _: _['client_id'] == hop['client_id'], log))
+        hop_this = log_this_client[0]
+        while True:
+            hop_pre = None
+            for hop_ in log_this_client:
+                if hop_['document.location'] == hop['document.referer']:
+                    if dateutil.parser.parse(hop_['date']) < dateutil.parser.parse(hop['date']):
+                        hop_pre = hop_
+            if is_ours(hop_pre['document.referrer']):
+                winning_links.append(hop_pre['document.referrer'])
+                break
+    return winning_links
 
-print(winning_links)
+
+if __name__ == '__main__':
+    print(find_winning_links('log.json'))
